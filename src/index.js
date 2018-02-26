b68dd4ab01fc6a9d0876dedb5b5eade7a03deab9
Dmitry Shaido <d.shaido@gmail.com>
2018-02-27 01:35:34 +0300

    task 8

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var count = 0;
+  var temp = base;
+  var multiplier = 2;
+  while ( temp > 1) {
+    while ( temp % multiplier == 0) {
+      temp = Math.floor(temp / multiplier);
+    }
+    multiplier += 1;
+  }
+
+  multiplier -= 1;
+
+  for (var i = multiplier; Math.floor(number / i) >= 1; i *= multiplier) {
+      count += Math.floor(number / i);
+    }
+  return count;
 }
