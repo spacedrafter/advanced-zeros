61804e571248315c9d05ac94fc7cb7373369a67f
Dmitry Shaido <d.shaido@gmail.com>
2018-02-28 20:59:54 +0300

    algorithm modificated

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  var count = 0;
+  var answer = 0;
   var temp = base;
   var multiplier = 2;
+  var factorize = [];
   while ( temp > 1) {
     while ( temp % multiplier == 0) {
       temp = Math.floor(temp / multiplier);
+      factorize.push(multiplier);
     }
     multiplier += 1;
   }
 
-  multiplier -= 1;
+  multiplier = factorize[factorize.length -1];
+
+  var exponent = 0;
+  for (var i = 0; i < factorize.length; i ++) {
+    if (factorize[i] == factorize[factorize.length - 1]) {
+      exponent += 1;
+    }
+  }
 
   for (var i = multiplier; Math.floor(number / i) >= 1; i *= multiplier) {
-      count += Math.floor(number / i);
+      answer += Math.floor(number / i);
     }
-  return count;
+
+  return Math.floor(answer / exponent);
 }
